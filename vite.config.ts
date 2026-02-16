04640612fd51a17ae991478352442d3ca1a9a643
Vengefulinsaan <rumman2454@gmail.com>
2026-02-17 01:31:03 +0600

    Initial commit: index

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,8 @@ import react from '@vitejs/plugin-react-swc';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/template-of-r3f-typeScript-with-vite/'
+  base: '/template-of-r3f-typeScript-with-vite/', // repo name
+  build: {
+    sourcemap: false // removes main.tsx 404
+  }
 });
